9cc7dd5b5d17e0f30d569713b46ab97466f8c4e6
ChenJiYuan <jiyuan.chen@easystack.cn>
2022-09-30 19:33:26 +0800

    :rocket:development

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,8 +1,63 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { Comment, Form, Input, Button } from 'antd';
+import { get$, post$ } from '../../shared/helpers';
+import { finalize, tap } from 'rxjs';
+
+const { TextArea } = Input;
+const { Item } = Form;
+
+const Editor = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = useCallback((values: Record<string, string>) => {
+    setLoading(true);
+    post$('/comment/create', {
+      content: values.content,
+      belong: 'path/test',
+    }).pipe(
+      finalize(() => setLoading(false)),
+    ).subscribe();
+  }, []);
+
+  return (
+    <Form
+      onFinish={onFinish}
+    >
+      <Item
+        name={'content'}
+      >
+        <TextArea rows={4} />
+      </Item>
+      <Item>
+        <Button htmlType="submit" loading={loading} type="primary">提交</Button>
+      </Item>
+    </Form>
+  );
+};
 
 const Home: FC = () => {
+
+  const getList = useCallback(() => {
+    get$('/comment/list', { belong: 'path/test' }).pipe(
+      tap(res => {
+        console.log(res);
+      }),
+    ).subscribe();
+  }, []);
+
+  useEffect(() => {
+    getList();
+  }, [getList]);
+
   return (
-    <div>Home</div>
+    <div>
+      <Comment
+        content={
+          <Editor />
+        }
+      />
+    </div>
   );
 };
 
